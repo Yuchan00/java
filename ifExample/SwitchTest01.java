d9aea7a9c0cb753847c9bc66842f78d583d5c3e2
Yuchan00 <104113593+Yuchan00@users.noreply.github.com>
2022-04-27 16:47:38 +1000

    Add files via upload

---
+++ ifExample/SwitchTest01.java
@@ -0,0 +1,32 @@
+package ifExample;
+
+public class SwitchTest01 {
+
+	public static void main(String[] args) {
+		// 조건문 - switch문
+		int num = (int)(math.random() * 6) + `;
+				
+		switch(num) {
+		case 1:
+			System.out.println("주사위 값은 1입니다");
+			break;
+		case 2:
+			System.out.println("주사위 값은 2입니다");
+			break;
+		case 3:
+			System.out.println("주사위 값은 3입니다");
+			break;
+		case 4:
+			System.out.println("주사위 값은 4입니다");
+			break;
+		case 5:
+			System.out.println("주사위 값은 5입니다");
+			break;
+		case 6:
+			System.out.println("주사위 값은 6입니다");
+			break;
+		}
+
+	}
+
+}
---
+++ ifExample/diceExample.java
@@ -0,0 +1,25 @@
+package ifExample;
+
+public class diceExample {
+
+	public static void main(String[] args) {
+		// 조건문 - if문
+		
+		
+		else if(num==1) {
+		    System.out.println("주사위 값은 1입니다");
+		else if(num==2) {
+			System.out.println("주사위 값은 2입니다");
+	    else if(num==3) {
+			System.out.println("주사위 값은 3입니다");
+		else if(num==4) {
+			System.out.println("주사위 값은 4입니다");
+		else if(num==5) {
+		    System.out.println("주사위 값은 5입니다");
+		else if(num==6) {
+			System.out.println("주사위 값은 6입니다");
+			
+   
+	}
+
+}
